9612439a23aac280a33a7a02e12eaae508aeda3c
Vaclav Blazek <vaclav.blazek@firma.seznam.cz>
2006-03-01 12:32:55 +0000

    autotools bootstrap
    git-svn-id: https://fastrpc.svn.sourceforge.net/svnroot/fastrpc/fastrpc/trunk@35 b39e363e-0249-4afc-b8ac-0517a591d126

---
+++ bootstrap.sh
@@ -0,0 +1,52 @@
+#
+# Fast RPC RPC library suports XML and binary format.
+# Copyright (C) 2004  Seznam.cz, a.s.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Seznam.cz, a.s.
+# Naskove 1, Praha 5, 15000, Czech Republic
+# http://www.seznam.cz, mailto:fastrpc@firma.seznam.cz
+#
+#
+# $Id: bootstrap.sh,v 1.1 2006-03-01 12:32:55 vasek Exp $
+#
+#
+# DESCRIPTION
+# Bootstrap for CVS checkout -- uses autotools to create all needed files.
+#
+# AUTHORS
+# Vaclav Blazek <blazek@firma.seznam.cz>
+#
+# HISTORY
+# 2004-09-24  (vasek)
+#             Created.
+#
+
+echo -n "Running aclocal... "
+aclocal
+echo "done."
+
+echo -n "Running libtoolize... "
+libtoolize --force
+echo "done."
+
+echo -n "Running automake... "
+automake --add-missing --gnu
+echo "done."
+
+echo -n "Running autoconf... "
+autoconf
+echo "done."
