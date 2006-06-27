0df139d9158b24d32e2d59d3bb9d7aa90d37d9a3
Vaclav Blazek <vaclav.blazek@firma.seznam.cz>
2006-06-27 12:52:53 +0000

    added omitted <memory> header
    git-svn-id: https://fastrpc.svn.sourceforge.net/svnroot/fastrpc/python/trunk@71 b39e363e-0249-4afc-b8ac-0517a591d126

--- pythonserver.cc
+++ pythonserver.cc
@@ -2,7 +2,7 @@
  * FastRPC - RPC protocol suport Binary and XML.
  * Copyright (C) 2005 Seznam.cz, a.s.
  *
- * $Id: pythonserver.cc,v 1.3 2006-06-27 12:38:16 vasek Exp $
+ * $Id: pythonserver.cc,v 1.4 2006-06-27 12:52:53 vasek Exp $
  *
  * AUTHOR      Vaclav Blazek <blazek@firma.seznam.cz>
  *
@@ -20,6 +20,7 @@
 #include <string>
 #include <map>
 #include <list>
+#include <memory>
 
 #include <Python.h>
 #include <structmember.h>
