7a8ff080e9b7cf708db5883cdde96c4b651245d6
Vaclav Blazek <vaclav.blazek@firma.seznam.cz>
2007-04-02 15:28:21 +0000

    added licence info
    git-svn-id: https://fastrpc.svn.sourceforge.net/svnroot/fastrpc/fastrpc/trunk@87 b39e363e-0249-4afc-b8ac-0517a591d126

--- src/frpcdefaultmethod.h
+++ src/frpcdefaultmethod.h
@@ -1,13 +1,31 @@
 /*
- * FILE          $Id: frpcdefaultmethod.h,v 1.1 2005-07-19 13:02:53 vasek Exp $
+ * FastRPC -- Fast RPC library compatible with XML-RPC
+ * Copyright (C) 2005-7  Seznam.cz, a.s.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Seznam.cz, a.s.
+ * Radlicka 2, Praha 5, 15000, Czech Republic
+ * http://www.seznam.cz, mailto:fastrpc@firma.seznam.cz
+ *
+ * FILE          $Id: frpcdefaultmethod.h,v 1.2 2007-04-02 15:28:21 vasek Exp $
  *
  * DESCRIPTION   
  *
  * AUTHOR        
  *              Miroslav Talasek <miroslav.talasek@firma.seznam.cz>
- *
- * Copyright (C) Seznam.cz a.s. 2005
- * All Rights Reserved
  *
  * HISTORY
  *       
