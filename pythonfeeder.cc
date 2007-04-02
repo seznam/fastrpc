ac1485368fb11fb8b363e1c15eb3fba0af6f4155
Vaclav Blazek <vaclav.blazek@firma.seznam.cz>
2007-04-02 15:46:18 +0000

    added licence info
    git-svn-id: https://fastrpc.svn.sourceforge.net/svnroot/fastrpc/python/trunk@90 b39e363e-0249-4afc-b8ac-0517a591d126

--- pythonfeeder.cc
+++ pythonfeeder.cc
@@ -1,8 +1,26 @@
 /*
- * FastRPC - RPC protocol suport Binary and XML.
- * Copyright (C) 2005 Seznam.cz, a.s.
+ * FastRPC -- Fast RPC library compatible with XML-RPC
+ * Copyright (C) 2005-7  Seznam.cz, a.s.
  *
- * $Id: pythonfeeder.cc,v 1.2 2006-06-27 12:04:06 vasek Exp $
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Seznam.cz, a.s.
+ * Radlicka 2, Praha 5, 15000, Czech Republic
+ * http://www.seznam.cz, mailto:fastrpc@firma.seznam.cz
+ *
+ * $Id: pythonfeeder.cc,v 1.3 2007-04-02 15:42:58 vasek Exp $
  *
  * AUTHOR      Vaclav Blazek <blazek@firma.seznam.cz>
  *
